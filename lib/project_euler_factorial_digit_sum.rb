89e0e6f4246e1a81e418ee5fdae73247a740628a
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-09 16:15:30 -0700

    commit tree ab952239bc4c872861d81d3fc80a2943b8508bfe

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,17 @@
 # code your solution here
+def factorial(input)
+
+    (2..input).reduce do |total, currentNum|
+        total * currentNum
+    end
+end
+
+def sum_of_digits(num)
+    num.to_s.split('').reduce do |sum, digit|
+        sum.to_i + digit.to_i
+    end
+end
+
+def factorial_digit_sum(input)
+    sum_of_digits(factorial(input))
+end
